5f76790272a4cb38e108c8b064662629ab7ab9b4
Asen Kiprov <assenkiprov@yahoo.com>
2020-04-10 18:48:24 +0300

    Configured mod initialization

--- Localizer/src/Localizer.cs
+++ Localizer/src/Localizer.cs
@@ -1,4 +1,21 @@
-﻿namespace Localizer
+﻿using ColossalFramework.Plugins;
+using ICities;
+
+namespace Localizer
 {
-	public class Localizer { }
+	public class Localizer : IUserMod
+	{
+		public string Name => "Localizer";
+
+		public string Description => "Cities Skylines Localization Mod";
+	}
+
+	public class Loader : LoadingExtensionBase
+	{
+		public override void OnCreated(ILoading loading)
+		{
+			DebugOutputPanel.AddMessage(PluginManager.MessageType.Message, "[Localizer] Mod is loading...");
+			base.OnCreated(loading);
+		}
+	}
 }
