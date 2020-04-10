1d4682b742452c545de8ae9981183b95d9459822
Asen Kiprov <assenkiprov@yahoo.com>
2020-04-10 21:49:24 +0300

    Moved to .Net Framework 3.5 for compatibility reasons

---
+++ Localizer/LoadingExtension.cs
@@ -0,0 +1,17 @@
+﻿using ColossalFramework.Plugins;
+using ICities;
+using System.Reflection;
+
+namespace Localizer
+{
+	public sealed class LoadingExtension : LoadingExtensionBase
+	{
+		private string ModName => Assembly.GetExecutingAssembly().GetName().Name;
+
+		public override void OnCreated(ILoading loading)
+		{
+			base.OnCreated(loading);
+			DebugOutputPanel.AddMessage(PluginManager.MessageType.Message, $"[{ModName}] Mod is loading...");
+		}
+	}
+}
---
+++ Localizer/Localizer.cs
@@ -0,0 +1,10 @@
+﻿using ICities;
+
+namespace Localizer
+{
+	public class Localizer : IUserMod
+	{
+		public string Description => "Cities Skylines Localization Mod";
+		public string Name => "Localizer";
+	}
+}
